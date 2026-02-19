0b0456b61a11048c36e25a907ae6f26a37b01576
youssef102111-sys <youssef102111@gmail.com>
2026-02-19 09:28:50 +0200

    Create mani.py

---
+++ mani.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.label import Label
+class MyApp(App):
+    def build(self):
+        return Label(text='Success!')
+if __name__ == '__main__':
+    MyApp().run()
